7bc6cf1ea48bbbc319c51e0afc80bd672d2c7a59
dantemessy <thepunishar1@gmail.com>
2020-02-24 22:57:52 +0200

    fixed

--- src/app.js
+++ src/app.js
@@ -1,63 +1,12 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import './app.scss';
+import Header from './components/header/header.js';
+import Footer from './components/footer/footer.js';
+import Main from './components/main/main.js';
 
 
 
-
-const Header = () => {
-  return (
-    <header>
-      <h1>Free Counter</h1>
-    </header>
-  );
-};
-
-const Footer = () => {
-  return (
-    <footer>
-      <p>Kawaiiii Footer</p>
-    </footer>
-  );
-};
-
-
-class Main extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      counter : 0,
-    };
-  }
-
-
-  handleIncrease = e =>{
-    console.log('adddd');
-    let counter = this.state.counter + 1;
-    this.setState({counter});
-  }
-
-  handleDecrease = e =>{
-    let counter = this.state.counter - 1;
-    this.setState({counter});
-  }
-
-  render(){
-    return(
-
-      <section>
-        <div id= 'number'>
-          {this.state.counter}
-        </div>
-        <div>
-        
-          <button id='left' onClick = {this.handleIncrease} >+</button>
-          <button id='right' onClick = {this.handleDecrease} >-</button>
-        </div>
-      </section>
-    );
-  }
-}
 
 class App extends React.Component {
   render(){
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
 
+
+// ====> method to ===> destination //
 ReactDOM.render(<App />, document.getElementById('root'));
 
